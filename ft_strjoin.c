ebe767d74a0b0461ecd3904952f0cf08ab2bb8c3
Boseong Kim <bosekim@c10r5s14.42seoul.kr>
2023-05-11 06:37:32 +0900

    add header

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bosekim <bosekim@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 06:21:49 by bosekim           #+#    #+#             */
+/*   Updated: 2023/05/11 06:24:12 by bosekim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include<stdlib.h>
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*result;
 	size_t	len;
-	
+
 	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	result = (char*)malloc(sizeof(char) * len);
+	result = (char *)malloc(sizeof(char) * len);
 	if (result == NULL)
-		return ((char *)NULL);
+		return ((char *) NULL);
 	ft_strlcpy(result, s1, ft_strlen(s1) + 1);
 	ft_strlcat(result, s2, ft_strlen(s2));
 	return (result);
