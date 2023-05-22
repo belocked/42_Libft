d26302b43bc91ea55922bef92369d0ed190bc768
belocked <bo-seng@naver.com>
2023-05-22 23:11:36 +0900

    modify

--- ft_split.c
+++ ft_split.c
@@ -12,34 +12,32 @@
 
 #include "libft.h"
 
-char    **ft_split(char const *s, char c)
+char	**ft_split_aloc_arr(char const *s, char c)
 {
-    char    **result;
+	char	**result;
+	size_t	s_idx;
+	size_t	w_cnt;
 
-    result = ft_split_aloc_arr(s,c);
-    if (result == NULL)
-        return (NULL);
-    if ()     
+	s_idx = 0;
+	w_cnt = 0;
+	while (s[s_idx])
+	{
+		if (s[s_idx] == c)
+			w_cnt = w_cnt + 1;
+		s_idx = s_idx + 1;
+	}
+	result = (char **)malloc(sizeof(char *) * (w_cnt + 2));
+	if (result == NULL)
+		return (NULL);
+	return (result);
 }
 
-
-char    **ft_split_aloc_arr(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    char    **result;
-    size_t     s_idx;
-    size_t     w_cnt;
-    
-    s_idx = 0;
-    w_cnt = 0;
-    while (s[s_idx])
-    {
-        if (s[s_idx] == c)
-            w_cnt = w_cnt + 1;
-        s_idx = s_idx + 1;
-    }
-    result = (char **)malloc(sizeof(char *) * (w_cnt + 2));
-    if (result == NULL)
-        return (NULL);
-    return (result);
-}
+	char	**result;
 
+	result = ft_split_aloc_arr(s, c);
+	if (result == NULL)
+		return (NULL);
+	if ()     
+}
