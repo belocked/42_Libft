a0550fe05fd6403a8cf4d80aeea2f4f29ecdb362
Boseong Kim <bosekim@c1r6s7.42seoul.kr>
2023-05-23 12:25:48 +0900

    add split

--- ft_split.c
+++ ft_split.c
@@ -41,3 +41,73 @@ char	**ft_split(char const *s, char c)
 		return (NULL);
 	if ()     
 }
+
+/*
+unsigned int	word_num(char const *s, char c)
+{
+	unsigned int	num;
+	int				i;
+
+	i = 0;
+	num = 0;
+	while (s[i] && s[i] == c)
+		++i;
+	while (s[i])
+	{
+		while (s[i] && s[i] != c)
+			++i;
+		while (s[i] && s[i] == c)
+			++i;
+		++num;
+	}
+	return (num);
+}
+
+char			*set(char *ret, char *start, size_t size)
+{
+	if (!(ret = (char *)malloc(size)))
+		return (char *)0;
+	ft_strlcpy(ret, start, size);
+	return (ret);
+}
+
+static char		**memfree(char **ret)
+{
+	size_t	i;
+
+	i = 0;
+	while (ret[i])
+	{
+		free(ret[i]);
+		i++;
+	}
+	free(ret);
+	return ((char **)NULL);
+}
+
+char			**ft_split(char const *str, char c)
+{
+	char			**ret;
+	unsigned int	w_num;
+	unsigned int	i;
+	char			*start;
+
+	i = 0;
+	w_num = word_num(str, c);
+	if (!(ret = (char **)malloc(sizeof(char *) * (w_num + 1))))
+		return (char **)0;
+	while (i < w_num)
+	{
+		while (*str && *str == c)
+			++str;
+		start = (char *)str;
+		while (*str && *str != c)
+			++str;
+		if (!(ret[i] = set(ret[i], start, str - start + 1)))
+			return (memfree(ret));
+		++i;
+	}
+	ret[i] = (char *)0;
+	return (ret);
+}
+*/
